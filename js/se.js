326e5f8663d3f595be10d728e6c328a0107065c5
gjq <825097564@qq.com>
2022-08-08 11:38:17 +0800

    第一次提交

---
+++ js/se.js
@@ -0,0 +1,20 @@
+class Tab {
+    constructor(id) {
+        this.main = document.querySelector(id);
+        this.lis = this.main.querySelectorAll('li');
+        this.sections = this.main.querySelectorAll('section');
+    }
+    toggleTab() {
+
+    }
+    addTab() {
+
+    }
+    removeTab() {
+
+    }
+    updateTab() {
+
+    }
+}
+new Tab('#tab');
